341527c47beede5610e8400b51feb407539a2dab
tdgeorge <github@tdgeorge.com>
2025-10-17 22:05:26 -0600

    Update game.js with complete game logic

--- flappy_bird/game.js
+++ flappy_bird/game.js
@@ -2,4 +2,29 @@ const GRAVITY = 0.25;
 const FLAP_STRENGTH = -7;
 const PIPE_WIDTH = 35;
 
-// Additional game logic...
+// Full game logic goes here
+// Example structure (You will need to fill in the complete logic):
+let bird = {
+    y: height / 2,
+    velocity: 0,
+    
+    flap: function() {
+        this.velocity += FLAP_STRENGTH;
+    },
+    
+    update: function() {
+        this.velocity += GRAVITY;
+        this.y += this.velocity;
+        // Additional game logic...
+    }
+};
+
+function setup() {
+    createCanvas(400, 600);
+}
+
+function draw() {
+    background(0);
+    bird.update();
+    // Draw bird, pipes, etc.
+}
